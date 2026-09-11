b2415cff3b1977702bb4fb75502551e9173c2627
Harumichi Maki <OCA2602IT0014@edu.oca.ac.jp>
2026-09-11 03:08:29 +0000

    0911

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
+  get "orders/new"
   get "mypage/show"
   devise_for :users
   resources :products
   resources :mypage, only: [:show] # ユーザ情報の詳細表示
   root to: "homes#top"
+  # 注文関連
+  #注文入力・注文作成
+  resources :orders, only: [:index, :new, :create] do 
+    collection do
+      post :confirm   # 注文確認
+    end
+
+    member do
+      get :complete  # 注文完了
+    end
+  end
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -16,4 +32,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  
+  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_28_005606) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_11_012003) do
+  create_table "orders", force: :cascade do |t|
+    t.string "address"
+    t.integer "count"
+    t.datetime "created_at", null: false
+    t.integer "product_id", null: false
+    t.integer "total_price"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name", null: false
     t.integer "price", null: false
     t.datetime "updated_at", null: false
+  end
 
-    create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
+    t.boolean "admin_flg", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "current_sign_in_at"
+    t.string "current_sign_in_ip"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
-    t.boolean "admin_flg", default: false, null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
-end
 
+  add_foreign_key "orders", "products"
+  add_foreign_key "orders", "users"
+end
